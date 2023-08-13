0ea416afbaba2e74940b8606752eaa849b56bfcc
leeyewon <rkdnl0610@gmail.com>
2023-08-14 05:47:13 +0900

    [docs]수정완

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Start from './page/Start';
+import Main from './page/Main';
+import Search from './page/Search';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Start /> } />
+          <Route path="/main" element={ <Main /> } />
+          <Route path="/search" element={ <Search /> } />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
